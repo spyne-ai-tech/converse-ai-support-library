c80147de2ba93aa943a68cbe054d9aa9bf9650f2
aakashraj1 <aakash.raj@spyne.ai>
2025-07-22 20:24:03 +0530

    Added Email for Package

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import CallComponent from "../components/Call/Call";
 import ChatbotComponent from "../components/chatbot";
+import EmailComponent from "../components/email";
 
 // Export types
 export type { CallProps } from "../types/Call";
@@ -10,6 +11,7 @@ export { useCallVapi } from "../hooks/useCallVapi";
 // Named exports
 export { CallComponent as Call };
 export { ChatbotComponent as Chatbot };
+export { EmailComponent as Email };
 
 // Default export (Call component as primary)
 export default CallComponent;
